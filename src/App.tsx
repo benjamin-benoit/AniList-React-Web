8657a9f514c610fee605804c23bca879c12dca4a
benjamin-benoit <benjaminbenoitfr@gmail.com>
2021-02-11 00:14:59 +0100

    files updates format

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import ROUTES from './constants'
+import Home from './pages/Home'
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path={ROUTES.HOME} component={HomePage} />
+        <Route exact path={ROUTES.HOME} component={Home} />
       </Switch>
     </Router>
   )
